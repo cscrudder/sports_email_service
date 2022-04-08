c3db0776933726cb6fdf87558d12049b2ed9ee43
cscrudder <cms447@georgetown.edu>
2022-04-08 10:56:03 -0400

    Adds the formatted time of the game to the printout
    I created a utilities file to add the time formatting function

--- dailygames.py
+++ dailygames.py
@@ -1,9 +1,10 @@
 from datetime import date
+from pytz import timezone
 import requests
 from dotenv import load_dotenv
 import os
 from standings import get_standings
-
+from utilities import time_formatter
 load_dotenv()
 
 # sets today's date
@@ -15,6 +16,11 @@ month = str(today.month)
 year = str(today.year)
 api_key = os.getenv('SPORTSRADAR_NHL_API')
 
+# checks if timezone env is set, if not, defaults to ET
+if os.getenv('TIMEZONE') is not None:
+    timezone = os.getenv('TIMEZONE')
+else:
+    timezone = 'ET'
 
 # code to get API data, syntax courtesy of Plotly
 schedule_url = 'http://api.sportradar.us/nhl/trial/v7/en/games/' + year + '/'+ month + '/'+ day + '/schedule.json?api_key=' + api_key
@@ -22,23 +28,26 @@ schedule_r = requests.get(schedule_url)
 schedule_data = schedule_r.json()
 print('\n')
 
-
-
 # imports standings
-standings = get_standings()
+standings_data = get_standings()
 
 # Loops through days' games and prints home and away teams
 for game in schedule_data['games']:
     # prints teams playing
-    print(' - ' + game['away']['name'] + ' at ' + game['home']['name'])
+    print(' - ' + game['away']['name'] + ' at ' + game['home']['name'] + ' (' + time_formatter(game['scheduled'], timezone) + ')')
 
-    
+    # Makes list of lists st [team name, win #, loss #]
+    standings = []
+    for conference in standings_data['conferences']:
+       for division in conference['divisions']:
+           for team in division['teams']:
+               standings.append([team['name'],team['wins'],team['losses']])
     
     # prints the teams' records
     for i in range(len(standings)):
         if standings[i][0] in game['away']['name']: #away team
             # each team has this [team name, win #, loss #] in standings
-            record_msg = '   - Record: ' + standings[i][0] + ' (' + str(standings[i][1]) + 'W-' + str(standings[i][2]) + 'L) // '
+            record_msg = '   - Records: ' + standings[i][0] + ' (' + str(standings[i][1]) + 'W-' + str(standings[i][2]) + 'L) | '
     for i in range(len(standings)):
         if standings[i][0] in game['home']['name']: #home team
             # each team has this [team name, win #, loss #] in standings
