33989309498b6f834e1d17b51b1295bb140e4d7b
jackfeingold <97628090+jackfeingold@users.noreply.github.com>
2022-04-08 09:33:21 -0400

    Jack branch (#1)
    * Add requirements, print standings lists, convert email service to .py
    
    * Update standings.py
    
    * Update standings.py
    
    * Update standings.py
    
    * Update standings.py
    
    * Update requirements.txt

--- standings.py
+++ standings.py
@@ -3,6 +3,9 @@ import requests
 from dotenv import load_dotenv
 import os
 import time
+from pprint import pprint
+import json
+
 
 def get_standings():
 
@@ -18,13 +21,125 @@ def get_standings():
     url = 'http://api.sportradar.us/nhl/trial/v7/en/seasons/' + season + '/REG/standings.json?api_key=' + api_key
     r = requests.get(url)
     standings_data = r.json()
+    #pprint(standings_data)
+    #pprint(standings_data["conferences"][0].keys())
+    #print(standings_data["conferences"][0]["divisions"][0].keys())
+    #print(standings_data["conferences"][0]["divisions"][0]["teams"][0].keys())
+    #print(len(standings_data["conferences"]))
 
-    # Makes list of lists st [team name, win #, loss #]
-    standings = []
-    for conference in standings_data['conferences']:
-        for division in conference['divisions']:
-            for team in division['teams']:
-                standings.append([team['name'],team['wins'],team['losses']])
+    return standings_data
 
-    return standings
 
+
+def nhl_conference_standings(standings_data):
+
+    """
+    This function requires input of a dictionary from the Sportsradar API with NHL league standings data.
+    The API can be accessed with the following URL (see below in python syntax):
+
+    'http://api.sportradar.us/nhl/trial/v7/en/seasons/' + season + '/REG/standings.json?api_key=' + api_key
+
+    In the above, the season part should be a year and the API key should be obtained by each developer.
+
+    When the data is returned from the API, store it in a variable as a dictionary and pass it into this function.
+    """
+
+    print(" ")
+    # conference standings
+    for conference in range(len(standings_data["conferences"])):
+
+        conf_teams = []
+        for x in standings_data["conferences"][conference]["divisions"]:
+            for team in x["teams"]:
+                teams = []
+                teams.append(team["rank"]["conference"])
+                teams.append(team["market"])
+                teams.append(team["name"])
+                teams.append(team["wins"])
+                teams.append(team["losses"])
+                teams.append(team["overtime_losses"])
+                teams.append(team["points"])
+                conf_teams.append(teams)
+        
+        conf_teams = sorted(conf_teams, key = lambda x: x[0])
+
+        
+        print(standings_data["conferences"][conference]["alias"], "CONFERENCE STANDINGS")
+        print(" ")
+        for item in conf_teams:
+            print(str(item[0]) + ".", str(item[1]), str(item[2]), "|", "Record:", str(item[3]) + "-" 
+            + str(item[4]) + "-" + str(item[5]), "|", "Points:", str(item[6]))
+
+        print(" ")
+
+def nhl_division_standings(standings_data):
+
+    """
+    This function requires input of a dictionary from the Sportsradar API with NHL league standings data.
+    The API can be accessed with the following URL (see below in python syntax):
+
+    'http://api.sportradar.us/nhl/trial/v7/en/seasons/' + season + '/REG/standings.json?api_key=' + api_key
+
+    In the above, the season part should be a year and the API key should be obtained by each developer.
+
+    When the data is returned from the API, store it in a variable as a dictionary and pass it into this function.
+    """
+
+    # division standings
+
+    for conference in range(len(standings_data["conferences"])):
+
+        
+        for division in range(len(standings_data["conferences"][conference]["divisions"])):
+            div_teams = []
+            for team in standings_data["conferences"][conference]["divisions"][division]["teams"]:
+                teams = []
+                teams.append(team["rank"]["division"])
+                teams.append(team["market"])
+                teams.append(team["name"])
+                teams.append(team["wins"])
+                teams.append(team["losses"])
+                teams.append(team["overtime_losses"])
+                teams.append(team["points"])
+                div_teams.append(teams)
+        
+            div_teams = sorted(div_teams, key = lambda x: x[0])
+
+        
+            print(standings_data["conferences"][conference]["divisions"][division]["alias"], "DIVISION STANDINGS")
+            print(" ")
+            for item in div_teams:
+                print(str(item[0]) + ".", str(item[1]), str(item[2]), "|", "Record:", str(item[3]) + "-" 
+                + str(item[4]) + "-" + str(item[5]), "|", "Points:", str(item[6]))
+
+            print(" ")
+
+
+if __name__ == "__main__":
+
+    standings = get_standings()
+
+    nhl_conference_standings(standings)
+    nhl_division_standings(standings)
+
+
+
+
+
+
+
+    ## Makes list of lists st [team name, win #, loss #]
+    #standings = []
+    #for conference in standings_data['conferences']:
+    #    for division in conference['divisions']:
+    #        for team in division['teams']:
+    #            standings.append([team['name'],team['wins'],team['losses']])
+#
+    #return standings
+
+#pprint(get_standings())
+
+#standings = get_standings()
+#
+#for x in range(len(standings)):
+#    print(str(x+1)+".", standings[x][0])
