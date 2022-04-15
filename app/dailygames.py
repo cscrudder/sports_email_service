9775170959f21f255dba586e2c8072a1aebb890e
cscrudder <cms447@georgetown.edu>
2022-04-15 11:52:22 -0400

    Refactored API pulls to simplify code.
    There were some weird merges in the schedule file. I had cleaned that up so its readable.

--- app/dailygames.py
+++ app/dailygames.py
@@ -1,40 +1,24 @@
-from datetime import date
 from pytz import timezone
-import requests
 from dotenv import load_dotenv
 import os
 
-from app.standings import get_standings
+from app.api_pulls import get_standings
+from app.api_pulls import get_schedule
 from app.utilities import time_formatter
 
-from standings import get_standings
-from utilities import time_formatter
+# imports standings
+standings_data = get_standings()
 
-load_dotenv()
+# imports schedule
+schedule_data = get_schedule()
 
-# sets today's date
-today = date.today()
-
-# Sets today's date variables automatically // calls API key from .env
-day = str(today.day)
-month = str(today.month)
-year = str(today.year)
-api_key = os.getenv('SPORTSRADAR_NHL_API')
 
 # checks if timezone env is set, if not, defaults to ET
+load_dotenv()
 if os.getenv('TIMEZONE') is not None:
     timezone = os.getenv('TIMEZONE')
 else:
     timezone = 'ET'
-
-# code to get API data, syntax courtesy of Plotly
-schedule_url = 'http://api.sportradar.us/nhl/trial/v7/en/games/' + year + '/'+ month + '/'+ day + '/schedule.json?api_key=' + api_key
-schedule_r = requests.get(schedule_url)
-schedule_data = schedule_r.json()
-print('\n')
-
-# imports standings
-standings_data = get_standings()
 
 # Loops through days' games and prints home and away teams
 for game in schedule_data['games']:
@@ -92,96 +76,28 @@ rivalries = [['Ottawa Senators','Toronto Maple Leafs',2],['Buffalo Sabres','Toro
     ['Calgary Flames','Edmonton Oilers',2],['Calgary Flames','Vancouver Canucks',1],['Anaheim Ducks','Los Angeles Kings',1], \
     ['Los Angeles Kings','San Jose Sharks',1],['San Jose Sharks','Vegas Golden Knights',1],['Calgary Flames','Winnipeg Jets',2],\
     ['Edmonton Oilers','Winnipeg Jets',1],['Chicago Blackhawks','Detroit Red Wings',2],['Colorado Avalanche','Detroit Red Wings',2], \
-
-import requests
-from dotenv import load_dotenv
-import os
-from app.standings import get_standings
-from pprint import pprint
+    ['Detroit Red Wings','Toronto Maple Leafs',3]]
 
 
 
-def load_schedule():
+# # still need to resolve how to show the time of the game, looks wonky af in the data
+# def show_schedule(schedule_data):
 
-    load_dotenv()
-
-    # sets today's date
-    today = date.today()
-
-    # Sets today's date variables automatically // calls API key from .env
-    day = str(today.day)
-    month = str(today.month)
-    year = str(today.year)
-    api_key = os.getenv('SPORTSRADAR_NHL_API')
-
-
-    # code to get API data, syntax courtesy of Plotly
-    schedule_url = 'http://api.sportradar.us/nhl/trial/v7/en/games/' + year + '/'+ month + '/'+ day + '/schedule.json?api_key=' + api_key
-    schedule_r = requests.get(schedule_url)
-    schedule_data = schedule_r.json()
-    print('\n')
-    #print(len(schedule_data["games"]))
-    return schedule_data
-
-
-# still need to resolve how to show the time of the game, looks wonky af in the data
-def show_schedule(schedule_data):
-
-    print(schedule_data["date"], "SCHEDULE:")
-    for game in schedule_data["games"]:
-        print(game["home"]["name"], "host the", game["away"]["name"])
-        print(game["away"]["alias"], "@", game["home"]["alias"])
-        print("Scheduled for:", game["scheduled"])
-        broadcasts = []
-        for broadcast in game["broadcasts"]:
-            broadcasts.append(broadcast["network"])
+#     print(schedule_data["date"], "SCHEDULE:")
+#     for game in schedule_data["games"]:
+#         print(game["home"]["name"], "host the", game["away"]["name"])
+#         print(game["away"]["alias"], "@", game["home"]["alias"])
+#         print("Scheduled for:", game["scheduled"])
+#         broadcasts = []
+#         for broadcast in game["broadcasts"]:
+#             broadcasts.append(broadcast["network"])
         
-        #there is a weird comma that shows up after "Watch on:" but I don't know how to get rid of it and still print the networks on the same line
-        print("Watch on:", *broadcasts, sep = ", ")
-        print(" ")
-        print("-----")
-        print(" ")
+#         #there is a weird comma that shows up after "Watch on:" but I don't know how to get rid of it and still print the networks on the same line
+#         print("Watch on:", *broadcasts, sep = ", ")
+#         print(" ")
+#         print("-----")
+#         print(" ")
 
 
 
-if __name__ == "__main__":
-
-    schedule_data = load_schedule()
-
-    # imports standings
-    standings = get_standings()
-
-    # Loops through days' games and prints home and away teams
-    for game in schedule_data['games']:
-        # prints teams playing
-        print(' - ' + game['away']['name'] + ' at ' + game['home']['name'])
-
-        
-        
-        # prints the teams' records
-        for i in range(len(standings)):
-            if standings[i][0] in game['away']['name']: #away team
-                # each team has this [team name, win #, loss #] in standings
-                record_msg = '   - Record: ' + standings[i][0] + ' (' + str(standings[i][1]) + 'W-' + str(standings[i][2]) + 'L) // '
-        for i in range(len(standings)):
-            if standings[i][0] in game['home']['name']: #home team
-                # each team has this [team name, win #, loss #] in standings
-                # adds home team to away team standings
-                record_msg = record_msg + standings[i][0] + ' (' + str(standings[i][1]) + 'W-' + str(standings[i][2]) + 'L)'
-        print(record_msg)   
-    
-        # prints info about the boradcast
-        broadcast_msg = '   - Broadcasts: '
-        for i in range(len(game['broadcasts'])):
-            broadcast_msg = broadcast_msg + (game['broadcasts'][i]['network'])
-            # if statements make sure to format the list correctly
-            if 'locale' in game['broadcasts'][i].keys():
-                broadcast_msg = broadcast_msg + ' (' + game['broadcasts'][i]['locale'].lower() + ')'
-            if i < (len(game['broadcasts'])-2):
-                broadcast_msg = broadcast_msg + ', '
-            if i == (len(game['broadcasts'])-2):
-                broadcast_msg = broadcast_msg + ' and ' 
-        print(broadcast_msg)
-        
-        print('')
 
--- app/function_tester.py
+++ app/function_tester.py
@@ -1,8 +1,12 @@
 
-from app.dailygames import load_schedule
-from app.dailygames import show_schedule
-from pprint import pprint
+# from app.dailygames import load_schedule
+# from app.dailygames import show_schedule
+# from pprint import pprint
 
-data = load_schedule()
+# data = load_schedule()
 
-show_schedule(data)
+# show_schedule(data)
+
+from datetime import date
+today = date.today().strftime("%b %d %Y")
+print(today)
