1be91352f25632abcc50025f219e53520d40b55a
jackfeingold <jpf99@georgetown.edu>
2022-04-20 17:30:16 -0400

    Add logic

--- app/game_recommendation.py
+++ app/game_recommendation.py
@@ -26,15 +26,61 @@ def featured_game(user, schedule_data):
     
     
     if user['affiliation'] != "League":
+
+        done = False
+
         for game in schedule_data['games']:
             # prints teams playing
             #print(' - ' + game['away']['name'] + ' at ' + game['home']['name'] + ' (' + time_formatter(game['scheduled'], timezone) + ')')
             if game['away']['name'] == user['affiliation'] or game['home']['name'] == user['affiliation']:
                 featured_game = game
+                done = True
+        if done == False:
+            rivalry_games = []
+            featured_game = None
+
+            for game in schedule_data["games"]:
+                teams = []
+                teams.append(game['away']['name'])
+                teams.append(game['home']['name'])
+                for matchup in rivalries:
+                    if (teams[0] in matchup) and (teams[1] in matchup):
+                        rivalry_games.append(game)
+            
+            print(rivalry_games)
+
+            if len(rivalry_games) == 1:
+                featured_game = game
+            elif len(rivalry_games) > 1:
+                primetime_games = []
+                for match in rivalry_games:
+                    gametime = time_formatter(match["scheduled"])
+                    #print(gametime[0])
+                    if int(gametime[0]) == 7 or int(gametime[0]) == 8 or int(gametime[0]) == 9:
+                        primetime_games.append(match)
+                    
+                if len(primetime_games) == 0:
+                    featured_game = rivalry_games[0]
+                else:
+                    featured_game = primetime_games[0]
+            
+            elif len(rivalry_games) == 0:
+                primetime_games = []
+                for match in schedule_data["games"]:
+                    gametime = time_formatter(match["scheduled"])
+                    #print(gametime[0])
+                    if int(gametime[0]) == 7 or int(gametime[0]) == 8 or int(gametime[0]) == 9:
+                        primetime_games.append(match)
+                    
+                if len(primetime_games) == 0:
+                    featured_game = None
+                else:
+                    featured_game = primetime_games[0]
+
 
     elif user['affiliation'] == "League":
 
-        rivalry_score = 1
+        
         rivalry_games = []
         featured_game = None
 
@@ -44,8 +90,7 @@ def featured_game(user, schedule_data):
             teams.append(game['home']['name'])
             for matchup in rivalries:
                 if (teams[0] in matchup) and (teams[1] in matchup):
-                    if matchup[2] > rivalry_score:
-                        rivalry_games.append(game)
+                    rivalry_games.append(game)
         
         print(rivalry_games)
 
@@ -73,7 +118,7 @@ def featured_game(user, schedule_data):
                     primetime_games.append(match)
                 
             if len(primetime_games) == 0:
-                featured_game = "There is no featured game today."
+                featured_game = None
             else:
                 featured_game = primetime_games[0]
 
--- app/users.py
+++ app/users.py
@@ -1,2 +1,2 @@
-users = [{"name":"Jack","email":"jack.feingold1@gmail.com","affiliation":"League"},
+users = [{"name":"Jack","email":"jack.feingold1@gmail.com","affiliation":"Detroit Red Wings"},
 {"name":"Colton","email":"cms447@georgetown.edu","affiliation":"Colorado Avalanche"}]
