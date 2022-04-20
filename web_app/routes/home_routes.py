f68ab93fa7edab73a5c23f1f827dd2cc34cb20b8
cscrudder <cms447@georgetown.edu>
2022-04-20 16:41:32 -0400

    Updates to website, now includes userform

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,6 +1,6 @@
 # web_app/routes/home_routes.py
 
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, jsonify, render_template, redirect, flash # FYI new imports
 
 home_routes = Blueprint("home_routes", __name__)
 
@@ -13,6 +13,14 @@ def home():
 def sample():
     return render_template("sample.html")
 
-@home_routes.route("/register")
+@home_routes.route("/register", methods=["GET","POST"])
 def register():
+
+    # if request.method == "GET":
+    #     print("URL PARAMS:", dict(request.args))
+    #     request_data = dict(request.args)
+    if request.method == "POST": # the form will send a POST
+        print("FORM DATA:", dict(request.form))
+        request_data = dict(request.form)
+
     return render_template("register.html")
