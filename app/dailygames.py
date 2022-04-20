77e0bc456bdbdbb4d0865fcb1bb584988200a014
jackfeingold <jpf99@georgetown.edu>
2022-04-20 17:22:00 -0400

    Write game recommendation function, add main condtional to dailygames

--- app/dailygames.py
+++ app/dailygames.py
@@ -6,62 +6,6 @@ from app.api_pulls import get_standings
 from app.api_pulls import get_schedule
 from app.utilities import time_formatter
 
-# imports standings
-standings_data = get_standings()
-
-# imports schedule
-schedule_data = get_schedule()
-
-
-# checks if timezone env is set, if not, defaults to ET
-load_dotenv()
-if os.getenv('TIMEZONE') is not None:
-    timezone = os.getenv('TIMEZONE')
-else:
-    timezone = 'ET'
-
-# Loops through days' games and prints home and away teams
-for game in schedule_data['games']:
-    # prints teams playing
-    print(' - ' + game['away']['name'] + ' at ' + game['home']['name'] + ' (' + time_formatter(game['scheduled'], timezone) + ')')
-
-    # Makes list of lists st [team name, win #, loss #]
-    standings = []
-    for conference in standings_data['conferences']:
-       for division in conference['divisions']:
-           for team in division['teams']:
-               standings.append([team['name'],team['wins'],team['losses']])
-    
-    # prints the teams' records
-    for i in range(len(standings)):
-        if standings[i][0] in game['away']['name']: #away team
-            # each team has this [team name, win #, loss #] in standings
-            record_msg = '   - Records: ' + standings[i][0] + ' (' + str(standings[i][1]) + 'W-' + str(standings[i][2]) + 'L) | '
-    for i in range(len(standings)):
-        if standings[i][0] in game['home']['name']: #home team
-            # each team has this [team name, win #, loss #] in standings
-            # adds home team to away team standings
-            record_msg = record_msg + standings[i][0] + ' (' + str(standings[i][1]) + 'W-' + str(standings[i][2]) + 'L)'
-    print(record_msg)   
- 
-    # prints info about the boradcast
-    broadcast_msg = '   - Broadcasts: '
-    for i in range(len(game['broadcasts'])):
-        broadcast_msg = broadcast_msg + (game['broadcasts'][i]['network'])
-        # if statements make sure to format the list correctly
-        if 'locale' in game['broadcasts'][i].keys():
-            broadcast_msg = broadcast_msg + ' (' + game['broadcasts'][i]['locale'].lower() + ')'
-        if i < (len(game['broadcasts'])-2):
-            broadcast_msg = broadcast_msg + ', '
-        if i == (len(game['broadcasts'])-2):
-            broadcast_msg = broadcast_msg + ' and ' 
-    print(broadcast_msg)
-    
-    print('')
-
-# Rivalry daya came from Wikipedia list: https://en.wikipedia.org/wiki/National_Hockey_League_rivalries
-# Significant rivalries were cross-referenced with this list: https://www.rookieroad.com/ice-hockey/top-10-nhl-rivalries-of-all-time/
-# Rivalries were assigned a score of 1 (mild), 2 (moderate to significant), or 3 (extreme)
 rivalries = [['Ottawa Senators','Toronto Maple Leafs',2],['Buffalo Sabres','Toronto Maple Leafs',1],['Boston Bruins','Buffalo Sabres',2],\
     ['Boston Bruins','Montreal Canadiens',3],['Boston Bruins','Tampa Bay Lightning',1],['Boston Bruins','Toronto Maple Leafs',2],\
     ['Florida Panthers','Tampa Bay Lightning',2],['Montreal Canadiens','Ottawa Senators',2], ['Montreal Canadiens','Toronto Maple Leafs',3], \
@@ -80,23 +24,92 @@ rivalries = [['Ottawa Senators','Toronto Maple Leafs',2],['Buffalo Sabres','Toro
 
 
 
-# # still need to resolve how to show the time of the game, looks wonky af in the data
-# def show_schedule(schedule_data):
 
-#     print(schedule_data["date"], "SCHEDULE:")
-#     for game in schedule_data["games"]:
-#         print(game["home"]["name"], "host the", game["away"]["name"])
-#         print(game["away"]["alias"], "@", game["home"]["alias"])
-#         print("Scheduled for:", game["scheduled"])
-#         broadcasts = []
-#         for broadcast in game["broadcasts"]:
-#             broadcasts.append(broadcast["network"])
+
+
+
+
+
+if __name__ == "__main__":
+
+
+    # imports standings
+    standings_data = get_standings()
+
+    # imports schedule
+    schedule_data = get_schedule()
+
+
+    # checks if timezone env is set, if not, defaults to ET
+    load_dotenv()
+    if os.getenv('TIMEZONE') is not None:
+        timezone = os.getenv('TIMEZONE')
+    else:
+        timezone = 'ET'
+
+    # Loops through days' games and prints home and away teams
+    for game in schedule_data['games']:
+        # prints teams playing
+        print(' - ' + game['away']['name'] + ' at ' + game['home']['name'] + ' (' + time_formatter(game['scheduled'], timezone) + ')')
+
+        # Makes list of lists st [team name, win #, loss #]
+        standings = []
+        for conference in standings_data['conferences']:
+            for division in conference['divisions']:
+                for team in division['teams']:
+                    standings.append([team['name'],team['wins'],team['losses']])
         
-#         #there is a weird comma that shows up after "Watch on:" but I don't know how to get rid of it and still print the networks on the same line
-#         print("Watch on:", *broadcasts, sep = ", ")
-#         print(" ")
-#         print("-----")
-#         print(" ")
+        # prints the teams' records
+        for i in range(len(standings)):
+            if standings[i][0] in game['away']['name']: #away team
+                # each team has this [team name, win #, loss #] in standings
+                record_msg = '   - Records: ' + standings[i][0] + ' (' + str(standings[i][1]) + 'W-' + str(standings[i][2]) + 'L) | '
+        for i in range(len(standings)):
+            if standings[i][0] in game['home']['name']: #home team
+                # each team has this [team name, win #, loss #] in standings
+                # adds home team to away team standings
+                record_msg = record_msg + standings[i][0] + ' (' + str(standings[i][1]) + 'W-' + str(standings[i][2]) + 'L)'
+        print(record_msg)   
+    
+        # prints info about the boradcast
+        broadcast_msg = '   - Broadcasts: '
+        for i in range(len(game['broadcasts'])):
+            broadcast_msg = broadcast_msg + (game['broadcasts'][i]['network'])
+            # if statements make sure to format the list correctly
+            if 'locale' in game['broadcasts'][i].keys():
+                broadcast_msg = broadcast_msg + ' (' + game['broadcasts'][i]['locale'].lower() + ')'
+            if i < (len(game['broadcasts'])-2):
+                broadcast_msg = broadcast_msg + ', '
+            if i == (len(game['broadcasts'])-2):
+                broadcast_msg = broadcast_msg + ' and ' 
+        print(broadcast_msg)
+        
+        print('')
+
+    # Rivalry daya came from Wikipedia list: https://en.wikipedia.org/wiki/National_Hockey_League_rivalries
+    # Significant rivalries were cross-referenced with this list: https://www.rookieroad.com/ice-hockey/top-10-nhl-rivalries-of-all-time/
+    # Rivalries were assigned a score of 1 (mild), 2 (moderate to significant), or 3 (extreme)
+
+
+
+
+    # # still need to resolve how to show the time of the game, looks wonky af in the data
+    # def show_schedule(schedule_data):
+
+    #     print(schedule_data["date"], "SCHEDULE:")
+    #     for game in schedule_data["games"]:
+    #         print(game["home"]["name"], "host the", game["away"]["name"])
+    #         print(game["away"]["alias"], "@", game["home"]["alias"])
+    #         print("Scheduled for:", game["scheduled"])
+    #         broadcasts = []
+    #         for broadcast in game["broadcasts"]:
+    #             broadcasts.append(broadcast["network"])
+            
+    #         #there is a weird comma that shows up after "Watch on:" but I don't know how to get rid of it and still print the networks on the same line
+    #         print("Watch on:", *broadcasts, sep = ", ")
+    #         print(" ")
+    #         print("-----")
+    #         print(" ")
 
 
 
---
+++ app/users.py
@@ -0,0 +1,2 @@
+users = [{"name":"Jack","email":"jack.feingold1@gmail.com","affiliation":"League"},
+{"name":"Colton","email":"cms447@georgetown.edu","affiliation":"Colorado Avalanche"}]
