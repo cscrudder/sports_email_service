6cf50460ce10d35e7dc2dfdac3482b646638009e
cscrudder <cms447@georgetown.edu>
2022-04-20 17:23:32 -0400

    Formatted userface with dropdowns/made it pretty

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -8,11 +8,11 @@ from web_app.routes.home_routes import home_routes
 
 load_dotenv()
 
-# SECRET_KEY = os.getenv("SECRET_KEY", default="super secret") # set this to something else on production!!!
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret") # set this to something else on production!!!
 
 def create_app():
     app = Flask(__name__)
-    # app.config["SECRET_KEY"] = SECRET_KEY
+    app.config["SECRET_KEY"] = SECRET_KEY
     app.register_blueprint(home_routes)
     return app
 
