539d3c43fdaa786c5c152b93bbe4a8218976cdca
cscrudder <cms447@georgetown.edu>
2022-04-21 15:40:11 -0400

    Adds google sheet to hold user data// adds calls from this sheet

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -19,13 +19,38 @@ def sample():
 
 @home_routes.route("/register", methods=["GET","POST"])
 def register():
-  
+    
     if request.method == "POST": # the form will send a POST
         print("FORM DATA:", dict(request.form))
 
         request_data = dict(request.form)
 
         if '@' in request_data['email'] and '.' in request_data['email'] and request_data['name'] != '':
+            
+            import gspread
+            from oauth2client.service_account import ServiceAccountCredentials
+
+
+            # use creds to create a client to interact with the Google Drive API
+            scope = ['https://spreadsheets.google.com/feeds',
+                    'https://www.googleapis.com/auth/drive']
+            creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+
+            client = gspread.authorize(creds)
+
+            # # Find a workbook by name and open the first sheet
+            # # Make sure you use the right name here.
+            sheet = client.open("nhl_daily_email_data").sheet1
+
+            row = [request_data['name'],request_data['email'],request_data['affiliation'],request_data['time_zone']]
+            index = 2
+            sheet.insert_row(row, index)
+            
+            
+            
+            
+            
+            
             flash("You have been signed up for the NHL Daily Briefing!", "success")
         else:
             flash("Invalid input. Try again.", "danger")
