c8167a3c3daa33de46efdb8eeade264cc89d47bb
cscrudder <cms447@georgetown.edu>
2022-04-25 23:42:11 -0400

    fixed lingering typo

--- app/functions.py
+++ app/functions.py
@@ -422,7 +422,7 @@ def get_user_data():
     # use creds to create a client to interact with the Google Drive API
     scope = ['https://spreadsheets.google.com/feeds',
             'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name('google-credentials.json', scope)
 
 
     client = gspread.authorize(creds)
